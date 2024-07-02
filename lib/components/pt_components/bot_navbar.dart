ec6727d9e929617116badd1a43ecc93115c5b7da
coar <m.masong14@gmail.com>
2024-07-02 12:14:45 +0800

    botnavbar for pt

---
+++ lib/components/pt_components/bot_navbar.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:kineticare/PhysicalTherapist/pt_chat.dart';
+import 'package:kineticare/PhysicalTherapist/pt_exerprograms.dart';
+import 'package:kineticare/PhysicalTherapist/pt_home.dart';
+import 'package:kineticare/PhysicalTherapist/pt_profile.dart';
+import 'package:kineticare/PhysicalTherapist/pt_sched.dart';
+import 'package:kineticare/components/app_images.dart';
+
+
+class BottomNavBarPt extends StatefulWidget {
+  const BottomNavBarPt({super.key});
+
+  @override
+  State<BottomNavBarPt> createState() => _BottomNavBarPtState();
+}
+
+class _BottomNavBarPtState extends State<BottomNavBarPt> {
+  List<Widget> screenList = [
+    const PtHome(),
+    const Appointment(),
+    const PtChat(),
+    const PtExerprograms(),
+    const PtProfile()
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      AppImages.currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Center(
+          child: screenList.elementAt(
+            AppImages.currentIndex.clamp(0, screenList.length - 1),
+          ),
+        ),
+        bottomNavigationBar: bottomNavigationBars(),
+      ),
+    );
+  }
+
+  Widget bottomNavigationBars() {
+    return BottomNavigationBar(
+      backgroundColor: Colors.white,
+      unselectedItemColor: const Color(0xFF00BFA6),
+      selectedIconTheme: const IconThemeData(
+        size: 40,
+        color: Colors.white,
+      ),
+      unselectedIconTheme: const IconThemeData(
+        size: 40,
+      ),
+      type: BottomNavigationBarType.fixed,
+      currentIndex: AppImages.currentIndex.clamp(0, screenList.length - 1),
+      selectedItemColor: const Color(0xFF00BFA6),
+      onTap: onItemTapped,
+      items: <BottomNavigationBarItem>[
+        _buildBottomNavigationBarItem(AppImages.home, 0),
+        _buildBottomNavigationBarItem(AppImages.calendar, 1),
+        _buildBottomNavigationBarItem(AppImages.chat, 2),
+        _buildBottomNavigationBarItem(AppImages.ptPrograms, 3),
+        _buildBottomNavigationBarItem(AppImages.profile, 4),
+      ],
+    );
+  }
+
+  BottomNavigationBarItem _buildBottomNavigationBarItem(
+      String imageAsset, int index) {
+    bool isSelected = AppImages.currentIndex == index;
+    return BottomNavigationBarItem(
+      icon: Stack(
+        alignment: Alignment.center,
+        children: [
+          if (isSelected)
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: const Color(0xFF5A8DEE).withOpacity(0.9),
+              ),
+            ),
+          ImageIcon(
+            AssetImage(imageAsset),
+            size: 40,
+          ),
+        ],
+      ),
+      label: '',
+    );
+  }
+}
