2d62cda9e961c5e751bee964b8ee21ab6d0161d5
coar <m.masong14@gmail.com>
2024-07-02 12:14:08 +0800

    therapist screen

---
+++ lib/PhysicalTherapist/pt_exerprograms.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PtExerprograms extends StatefulWidget {
+  const PtExerprograms({super.key});
+
+  @override
+  State<PtExerprograms> createState() => _PtExerprogramsState();
+}
+
+class _PtExerprogramsState extends State<PtExerprograms> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/PhysicalTherapist/pt_home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:kineticare/components/app_images.dart';
+
+class PtHome extends StatelessWidget {
+  const PtHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(75.0),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          title: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: Image.asset(
+                  AppImages.logoWOname,
+                  fit: BoxFit.contain,
+                  height: 75,
+                  width: 75,
+                ),
+              ),
+              Image.asset(
+                AppImages.appName,
+                fit: BoxFit.contain,
+                height: 20,
+              ),
+              const SizedBox(width: 125),
+              Image.asset(
+                AppImages.bell,
+                fit: BoxFit.contain,
+                height: 35,
+              ),
+              const SizedBox(width: 10),
+              Image.asset(
+                AppImages.profile,
+                fit: BoxFit.contain,
+                height: 35,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
